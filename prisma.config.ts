9e699a9cbe91345ded4c6420e4001413f89cb8b7
sid-methiya99 <siddharthmethiya180@gmail.com>
2026-03-04 00:29:20 +1100

    Some changes to satisify prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,14 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
+const isDev = process.env.NODE_ENV === 'development'
+const databaseUrl = isDev ? process.env.DIRECT_URL : process.env.DATABASE_URL
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
+    path: 'prisma/migrations',
   },
   datasource: {
-    url: env("DIRECT_URL"),
+    url: env(databaseUrl!),
   },
-});
+})
