b16bf823aaa194d9798801c8f3275003c69859bb
sid-methiya99 <siddharthmethiya180@gmail.com>
2026-03-04 00:33:52 +1100

    Some dumb things to make prisma work

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,12 @@
 import 'dotenv/config'
 import { defineConfig, env } from 'prisma/config'
 
-const isDev = process.env.NODE_ENV === 'development'
-const databaseUrl = isDev ? process.env.DIRECT_URL : process.env.DATABASE_URL
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env(databaseUrl!),
+    url: env('DATABASE_URL'),
   },
 })
